9642726f78e4c778d27209dee011c3c904ded7fb
computertomo <tomoki0908ueda@icloud.com>
2022-01-09 01:11:26 +0900

    first commit

---
+++ database/migrations/2022_01_08_122423_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('Main_task_id')->unsigned();
+            $table->integer('category_id')->unsigned();
+            $table->string('body', 200);
+            $table->string('start_at');
+            $table->string('finish_day');
+            $table->string('deleted_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
